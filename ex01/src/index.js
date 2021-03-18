92dcc33e31e8890426d16f07da9197b676b7d52c
salkorad <salko.radovovic@gmail.com>
2021-03-18 01:37:02 +0100

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+var myPet = {
+    species: "pitbull",
+    name: "Art",
+    legs: 4,
+    friends: ["Arci", "Medo"]
+};
+function myFunction(myObj) {
+    return myObj;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
